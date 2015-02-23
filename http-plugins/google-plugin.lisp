413b97d00dab3070d896bc4c34934c29af52359a
Hans-Peter Deifel <hans-peter.deifel@fau.de>
2015-02-23 14:54:47 +0100

    Fix google plugin

--- http-plugins/google-plugin.lisp
+++ http-plugins/google-plugin.lisp
@@ -10,19 +10,26 @@
 
 (defparameter *google-url* "http://www.google.com/search")
 
+(defparameter *user-agent*
+  "Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0 Iceweasel/31.4.0"))
+
 (defun get-google-matches (search)
   (let* ((html (drakma:http-request *google-url*
                                     :parameters `(("q" . ,search))
-                                    :user-agent "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; de-de) AppleWebKit/535+ (KHTML, like Gecko) Version/5.0 Safari/535.4+ Lispbot"))
+                                    :user-agent *user-agent*))
          (document (chtml:parse html (cxml-stp:make-builder)))
          (res nil))
-    (stp:do-recursively (a document)
-      (when (and (typep a 'stp:element)
-                 (equal (stp:local-name a) "a")
-                 (equal (stp:attribute-value a "class") "l"))
-        (push (cons (stp:string-value a)
-                    (stp:attribute-value a "href"))
-              res)))
+    (stp:do-recursively (elem document)
+      (when (and (typep elem 'stp:element)
+                 (equal (stp:local-name elem) "h3")
+                 (equal (stp:attribute-value elem "class") "r"))
+        (alexandria:when-let
+            (a (stp:find-child-if
+                (lambda (a) (and (typep a 'stp:element)
+                                 (equal (stp:local-name a) "a")))
+                elem))
+          (push (cons (stp:string-value a)
+                      (stp:attribute-value a "href")) res))))
     (reverse res)))
 
 (defcommand google ((pl google-plugin) arg1 &rest args)
